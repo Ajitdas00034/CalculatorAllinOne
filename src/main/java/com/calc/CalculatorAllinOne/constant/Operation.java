4845b6352df43eaca4e80827082a95f346078fa5
Ajit Das <ajitdas.csk@gmail.com>
2021-02-27 18:40:43 +0530

    percentage and remainder calculation logic implemented

--- src/main/java/com/calc/CalculatorAllinOne/constant/Operation.java
+++ src/main/java/com/calc/CalculatorAllinOne/constant/Operation.java
@@ -2,11 +2,15 @@ package com.calc.CalculatorAllinOne.constant;
 
 public class Operation {
 
+	
 	public static String BASIC_MATH_OPERATION = "BASIC_MATH_OPERATION";
 	public static String ADDITION = "ADDITION";
 	public static String SUBSTRACTION = "SUBSTRACTION";
 	public static String MULTIPLICATION = "MULTIPLICATION";
 	public static String DIVISION = "DIVISION";
+	public static String PERCENTAGE = "PERCENTAGE";
+	public static String REMAINDER = "REMAINDER";
+	
 
 	public static String LENGTH_OPERATION = "Length Operation";
 
